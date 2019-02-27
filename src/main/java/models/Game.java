fe6ac8f549dc65393e7e26ff2ae3e6d0eff5bfa8
Deathblade226 <48025600+Deathblade226@users.noreply.github.com>
2019-02-26 20:04:50 -0700

    Add files via upload

--- src/main/java/models/Game.java
+++ src/main/java/models/Game.java
@@ -2,4 +2,32 @@ package models;
 
 public class Game {
 
+private String name;
+private Player[] players = new Player[2];
+
+public Game(String name) {
+	this.setName(name);}
+
+public String getName() {
+return name;}
+
+public void setName(String name) {
+	this.name = name;}
+
+public Player[] getPlayers() {
+return players;}
+
+public void setPlayers(Player[] players) {
+	this.players = players;}
+
+@Override
+public String toString() {
+	StringBuilder builder = new StringBuilder();
+	builder.append(getName());
+	return builder.toString();}
+
+
+
+
+
 }
